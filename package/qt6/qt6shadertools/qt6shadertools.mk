94cd7e80f21a08ff786ef1e2d0e29b9ca6785546
Adam Duskett <adam.duskett@amarulasolutions.com>
2023-10-13 15:56:06 +0200

    package/qt6/qt6shadertools: new package
    Based off of previous work by Jesse Van Gavere found here:
    https://patchwork.ozlabs.org/project/buildroot/patch/20230808172231.1114696-1-jesse.vangavere@scioteq.com/
    
    Changes:
      - Select BR2_PACKAGE_HOST_QT6BASE_GUI
      - Select BR2_PACKAGE_QT6BASE_GUI
      - Add host-qt6base to HOST_QT6SHADERTOOLS_DEPENDENCIES
    Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>

---
+++ package/qt6/qt6shadertools/qt6shadertools.mk
@@ -0,0 +1,49 @@
+################################################################################
+#
+# qt6shadertools
+#
+################################################################################
+
+QT6SHADERTOOLS_VERSION = $(QT6_VERSION)
+QT6SHADERTOOLS_SITE = $(QT6_SITE)
+QT6SHADERTOOLS_SOURCE = qtshadertools-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6SHADERTOOLS_VERSION).tar.xz
+QT6SHADERTOOLS_INSTALL_STAGING = YES
+QT6SHADERTOOLS_SUPPORTS_IN_SOURCE_BUILD = NO
+
+QT6SHADERTOOLS_CMAKE_BACKEND = ninja
+
+QT6SHADERTOOLS_LICENSE = \
+	GPL-3.0-only WITH Qt-GPL-exception-1.0, \
+	GPL-2.0-only or LGPL-3.0-only, \
+	GFDL-1.3-no-invariants-only, \
+	Apache-2.0 or MIT, \
+	BSD-3-Clause, \
+	BSD-2-Clause, \
+	Apache-2.0, \
+	GPL-3.0-or-later WITH Bison-Exception-2.2
+
+QT6SHADERTOOLS_LICENSE_FILES = \
+	LICENSES/BSD-3-Clause.txt \
+	LICENSES/GFDL-1.3-no-invariants-only.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-only.txt \
+	LICENSES/LGPL-3.0-only.txt \
+	LICENSES/Qt-GPL-exception-1.0.txt \
+	src/3rdparty/SPIRV-Cross/KHRONOS-LICENSE.txt \
+	src/3rdparty/SPIRV-Cross/LICENSE \
+	src/3rdparty/glslang/LICENSE.txt
+
+QT6SHADERTOOLS_CONF_OPTS = \
+	-DBUILD_WITH_PCH=OFF \
+	-DQT_BUILD_EXAMPLES=OFF \
+	-DQT_BUILD_TESTS=OFF
+
+QT6SHADERTOOLS_DEPENDENCIES = \
+	host-pkgconf \
+	qt6base \
+	host-qt6shadertools
+
+HOST_QT6SHADERTOOLS_DEPENDENCIES = host-qt6base
+
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))
